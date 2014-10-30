3dc52a6c513257ddc8fb9f8af9d8ce613f667a0e
pbelko <belkop43@students.rowan.edu>
2014-10-30 11:32:10 -0400

    PrimeNumber

---
+++ PrimeNumbers.py
@@ -0,0 +1,20 @@
+__author__ = 'Pat'
+import sys
+
+def isPrime(max):
+    for x in range (2, max):
+        if(max%x == 0):
+            return 0
+    return 1
+
+while(1):
+    print('Enter 0 to quit')
+    number = int(input('Please enter a number: '))
+
+    if(number == 0):
+        sys.exit('')
+
+    for num in range(2, number):
+        if(isPrime(num) == 1):
+            print(int(num))
+
