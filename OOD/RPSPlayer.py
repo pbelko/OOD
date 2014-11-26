f3913c9593133c3ee857e0b6c037fa9072a17eeb
pbelko <belkop43@students.rowan.edu>
2014-11-26 10:27:17 -0500

    Update RPSPlayer.py

--- OOD/RPSPlayer.py
+++ OOD/RPSPlayer.py
@@ -12,6 +12,8 @@ class RPSPlayer(Player.Player):
     paper_cut = .6
     total = 10
     name = None
+    p1 = None
+    p2 = None
 
     # finds a player and his information
     def find_player(self, person):
@@ -48,25 +50,30 @@ class RPSPlayer(Player.Player):
     def match_results(self, person, played):
         player = self.find_player(person)
 
-        if(played == "rock"):
+        if(played == 0):
             player[1] = player[1] + 1
-        elif(played == "paper"):
+        elif(played == 1):
             player[2] = player[2] + 1
-        elif(played == "scissors"):
+        elif(played == 2):
             player[3] = player[3] + 1
 
     # implementing observer method
     # string checks will need to change to what messages actually will be
     def notify(self, msg):
-        split_message = str(msg).split(" ")
-
-        if split_message[1] == "picked":
-            self.match_results(split_message[0].lower(),split_message[2].lower())
-        elif split_message[1] == "playing" :
-            if split_message[0] == self.name:
-                self.me_playing_against(split_message[2].lower())
-            else:
-                self.me_playing_against(split_message[0].lower())
+        if msg.is_match_start_message():
+            p_list = msg.get_players()
+            self.p1 = p_list[0].name
+            self.p2 = p_list[1].name
+        elif msg.is_round_end_message():
+            if not self.p1 == self.name:
+                self.match_results(self.p1, msg.get_info()[0][0])
+            if not self.p2 == self.name:
+                self.match_results(self.p2, msg.get_info()[0][1])
+        elif msg.is_round_start_message():
+            if self.p1 == self.name:
+                self.me_playing_against(self.p2)
+            elif self.p2 == self.name:
+                self.me_playing_against(self.p1)
 
     # returns my move, based on what the other player chooses most often
     def play(self, position=None):
@@ -98,13 +105,29 @@ class RpsStrategy(object):
 
 if __name__ == "__main__":
     player = RPSPlayer()
+    player.set_name("Pat")
     opponent = RPSPlayer()
-    players = [opponent,player]
+    opponent.set_name("Tony")
+    other = RPSPlayer()
+    other.set_name("Baliga")
+    players = [opponent,other]
     fake_moves = (1,2)
     fake_result = (0,1)
+
+    ##Tests functionality while other play
+    player.notify(Message.Message.get_match_start_message(players))
+    player.notify(Message.Message.get_round_start_message(players))
+    player.notify(Message.Message.get_round_end_message(players,fake_moves,fake_result))
+    player.notify(Message.Message.get_match_end_message(players, fake_result))
+    print ("Moves during others: "+str(player.player_list))
+
+    ##Tests functionality when we are one of the players
+    players = [opponent, player]
 
     player.notify(Message.Message.get_match_start_message(players))
     player.notify(Message.Message.get_round_start_message(players))
     move = player.play()
     print ("Move played: ", move)
     player.notify(Message.Message.get_round_end_message(players,fake_moves,fake_result))
+    player.notify(Message.Message.get_match_end_message(players, fake_result))
+    print ("Moves during ours: "+str(player.player_list))
