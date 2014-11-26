b39ae5036f76dd4540f90a557157c516e954ca8a
pbelko <belkop43@students.rowan.edu>
2014-11-26 10:28:08 -0500

    Updated messaging system

--- OOD/rpsPlayer.py
+++ OOD/rpsPlayer.py
@@ -1,80 +1,133 @@
 __authors__ = 'Pat and Tony'
 
 import random
+import Player
+import Message
 
-class rpsPlayer(Player):
-    #self variables
-    playerList = []
-    rockCut = .3
-    paperCut = .6
+
+class RPSPlayer(Player.Player):
+    # self variables
+    player_list = []
+    rock_cut = .3
+    paper_cut = .6
     total = 10
     name = None
+    p1 = None
+    p2 = None
 
-    #finds a player and his information
-    def findPlayer(self, person):
-        foundPlayer = False
-        player = [person,0,0,0]
+    # finds a player and his information
+    def find_player(self, person):
+        found_player = False
+        player = [person, 0, 0, 0]
 
-        for i in range(0,len(self.playerList)):
-            if(self.playerList[i][0] == person):
-                player = self.playerList[i]
-                foundPlayer = True
+        for i in range(0, len(self.player_list)):
+            if (self.player_list[i][0] == person):
+                player = self.player_list[i]
+                found_player = True
                 break
 
-        if(not foundPlayer):
-            self.playerList.append(player)
+        if(not found_player):
+            self.player_list.append(player)
 
         return player
 
-    #gets the data for who I am playing against
-    #sets my odds to play against his pattern
-    def mePlayingAgainst(self, person):
-        player = self.findPlayer(person)
-        global total, rockCut, paperCut
+    # gets the data for who I am playing against
+    # sets my odds to play against his pattern
+    def me_playing_against(self, person):
+        player = self.find_player(person)
+        global total, rock_cut, paper_cut
 
-        rockCount = player[1]
-        paperCount = player[2]
-        scissorCount = player[3]
-        total = rockCount + paperCount + scissorCount
+        rock_count = player[1]
+        paper_count = player[2]
+        scissor_count = player[3]
+        total = rock_count + paper_count + scissor_count
 
         if(total >= 5):
-            rockCut = rockCount/total
-            paperCut = paperCount/total
+            rock_cut = rock_count/total
+            paper_cut = paper_count/total
 
-    #keeps track of what a player picks
-    def matchResults(self, person, played):
-        player = self.findPlayer(person)
+    # keeps track of what a player picks
+    def match_results(self, person, played):
+        player = self.find_player(person)
 
-        if(played == "rock"):
+        if(played == 0):
             player[1] = player[1] + 1
-        elif(played == "paper"):
-            player[2] = player[2] +1
-        elif(played == "scissors"):
+        elif(played == 1):
+            player[2] = player[2] + 1
+        elif(played == 2):
             player[3] = player[3] + 1
 
-    #implementing observer method
-    #string checks will need to change to what messages actually will be
+    # implementing observer method
+    # string checks will need to change to what messages actually will be
     def notify(self, msg):
-        splitMsg = (""+msg).split(" ")
+        if msg.is_match_start_message():
+            p_list = msg.get_players()
+            self.p1 = p_list[0].name
+            self.p2 = p_list[1].name
+        elif msg.is_round_end_message():
+            if not self.p1 == self.name:
+                self.match_results(self.p1, msg.get_info()[0][0])
+            if not self.p2 == self.name:
+                self.match_results(self.p2, msg.get_info()[0][1])
+        elif msg.is_round_start_message():
+            if self.p1 == self.name:
+                self.me_playing_against(self.p2)
+            elif self.p2 == self.name:
+                self.me_playing_against(self.p1)
 
-        if(splitMsg[1] == "picked"):
-            self.matchResults(splitMsg[0].lower(),splitMsg[2].lower())
-        elif(splitMsg[1] == "playing"):
-            if(splitMsg[0] == self.name):
-                self.mePlayingAgainst(splitMsg[2].lower())
-            else:
-                self.mePlayingAgainst(splitMsg[0].lower())
-
-    #returns my move, based on what the other player chooses most often
+    # returns my move, based on what the other player chooses most often
     def play(self, position=None):
         weight = random.random()
 
-        if(weight < paperCut):
+        if(weight < self.paper_cut):
             return 2
-        elif((weight-paperCut) < rockCut):
+        elif((weight-self.paper_cut) < self.rock_cut):
             return 1
         else:
             return 0
 
     def get_name(self):
         return self.name
+
+class RpsStrategy(object):
+     # returns my move, based on what the other player chooses most often
+     @staticmethod
+     def play(opponents_moves):
+        count = [0, 0, 0]
+        weight = random.random()
+
+        if weight < paper_cut:
+            return 2
+        elif weight-paper_cut < rock_cut:
+            return 1
+        else:
+            return 0
+
+if __name__ == "__main__":
+    player = RPSPlayer()
+    player.set_name("Pat")
+    opponent = RPSPlayer()
+    opponent.set_name("Tony")
+    other = RPSPlayer()
+    other.set_name("Baliga")
+    players = [opponent,other]
+    fake_moves = (1,2)
+    fake_result = (0,1)
+
+    ##Tests functionality while other play
+    player.notify(Message.Message.get_match_start_message(players))
+    player.notify(Message.Message.get_round_start_message(players))
+    player.notify(Message.Message.get_round_end_message(players,fake_moves,fake_result))
+    player.notify(Message.Message.get_match_end_message(players, fake_result))
+    print ("Moves during others: "+str(player.player_list))
+
+    ##Tests functionality when we are one of the players
+    players = [opponent, player]
+
+    player.notify(Message.Message.get_match_start_message(players))
+    player.notify(Message.Message.get_round_start_message(players))
+    move = player.play()
+    print ("Move played: ", move)
+    player.notify(Message.Message.get_round_end_message(players,fake_moves,fake_result))
+    player.notify(Message.Message.get_match_end_message(players, fake_result))
+    print ("Moves during ours: "+str(player.player_list))
